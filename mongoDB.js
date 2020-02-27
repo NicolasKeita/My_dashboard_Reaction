39284c679e371db7c84cd49c86052fd4b1d0a8fd
hugo.lacour <hugo.lacour@epitech.eu>
2020-02-27 09:34:46 +0100

    [DEL] usless file

--- mongoDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema  = mongoose.Schema({
-    name : String,
-    email : String,
-    password : String
-})
-
-module.exports = mongoose.model('user', schema);
