37ca2fb6f966b21a72f73186eb81c064d2816a79
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-02-27 03:27:08 +0100

    fix: class SimpleEntity remove extra variable

--- TestArea/app/src/main/java/com/example/testarea/SimpleEntity.java
+++ TestArea/app/src/main/java/com/example/testarea/SimpleEntity.java
@@ -2,26 +2,10 @@ package com.example.testarea;
 
 public class SimpleEntity {
     private String quantity;
-    private Boolean success;
-    private String source;
-    private String date;
-    private String str;
-    private String sym;
-    private String name;
-    private String sym2;
-    private String name2;
 
 
     public SimpleEntity(Boolean success, String source, String date, String str, String sym, String name, String sym2, String name2, String quantity) {
         this.quantity = quantity;
-        this.success = success;
-        this.source = source;
-        this.date = date;
-        this.str = str;
-        this.sym = sym;
-        this.name = name;
-        this.sym2 = sym2;
-        this.name2 = name2;
     }
 
     public String getQuantity() {
