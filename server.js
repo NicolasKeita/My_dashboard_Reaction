45e6131e54f239484d2c12f0264f809494268f37
hugo.lacour <hugo.lacour@epitech.eu>
2020-03-01 14:54:30 +0100

    [MOD] acces token

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.post('/register', async (req, res) => {
 
 app.get('/trello', (req, res) => {
     const options = {
-            url: 'https://api.trello.com/1/members/me/boards?key=45f4bc4d0a4abe2a27ec5d80c8a863e3&token=c7b1ebc16a3e174053ac7adccdd43845584c377ff64a04176d804a37dfabd042',
+            url: 'https://api.trello.com/1/members/me/boards?key=45f4bc4d0a4abe2a27ec5d80c8a863e3&token=dfd8ac5263e85fda855c31e86a0fcb0f2f82efe62c64ee650c2b72a7352c732a',
             method: 'GET',
             headers: {
                 'key': '45f4bc4d0a4abe2a27ec5d80c8a863e3',
-                'token': 'c7b1ebc16a3e174053ac7adccdd43845584c377ff64a04176d804a37dfabd042'
+                'token': 'dfd8ac5263e85fda855c31e86a0fcb0f2f82efe62c64ee650c2b72a7352c732a'
             }
         };
 
@@ -45,9 +45,11 @@ app.get('/trello', (req, res) => {
         });
 });
 
+//jZppEVS5SHVTEMkgClvE7eHzqYWZ1apKMPwfjshi
+
 app.get('/pictures', (req, res) => {
     const options = {
-            url: ' https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY ',
+            url: ' https://api.nasa.gov/planetary/apod?api_key=TbofUQJC5HtgwnLV2w0TgNHhcN9tSQOsFpRWnz1x ',
             method: 'GET',
             headers: {
                 'api_key': 'TbofUQJC5HtgwnLV2w0TgNHhcN9tSQOsFpRWnz1x'
@@ -62,7 +64,7 @@ app.get('/pictures', (req, res) => {
 
 app.get('/mars', (req, res) => {
     const options = {
-            url: 'https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0',
+            url: 'https://api.nasa.gov/insight_weather/?api_key=TbofUQJC5HtgwnLV2w0TgNHhcN9tSQOsFpRWnz1x&feedtype=json&ver=1.0',
             method: 'GET',
             headers: {
                 'version': '1.0',
@@ -116,12 +118,12 @@ app.get('/pollution', (req, res) => {
 
 app.get('/lol', (req, res) => {
     const options = {
-            url: 'https://euw1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-4e57278e-7056-497c-a474-fe8afa6091a5',
+            url: 'https://euw1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-ff838aff-1293-4ea9-a7f9-efd9d7281d69',
             method: 'GET',
             headers: {
                 "Origin": "https://developer.riotgames.com",
                 "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                "X-Riot-Token": "RGAPI-4e57278e-7056-497c-a474-fe8afa6091a5",
+                "X-Riot-Token": "RGAPI-ff838aff-1293-4ea9-a7f9-efd9d7281d69",
                 "Accept-Language": "en-US,en;q=0.5",
                 "User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"
             }
