9d3b0e9228eab2b51b9865c846dcf759637477e2
Lucas Rolland <lucas.rolland@epitech.eu>
2020-03-14 17:17:36 -0400

    client mobile, envoie mail quand bitcoin + ou - 50

--- client_mobile/app/src/main/java/com/example/area/BitcoinActivity.java
+++ client_mobile/app/src/main/java/com/example/area/BitcoinActivity.java
@@ -15,8 +15,10 @@ import org.json.JSONObject;
 
 import java.util.Objects;
 
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class BitcoinActivity extends AppCompatActivity implements View.OnClickListener {
@@ -35,13 +37,14 @@ public class BitcoinActivity extends AppCompatActivity implements View.OnClickLi
         final Request request = new Request.Builder()
                 .url("http://10.0.2.2:8080/bitcoin")
                 .build();
-        Response response;
+        final Response[] response = new Response[1];
         try {
-            response = httpClient.newCall(request).execute();
-            String jsonData = Objects.requireNonNull(response.body()).string();
+            response[0] = httpClient.newCall(request).execute();
+            String jsonData = Objects.requireNonNull(response[0].body()).string();
             JSONObject object = new JSONObject(jsonData);
             String quantity = object.getString("to_quantity");
             testBitcoin.setText("1€ = "+quantity+" btc");
+            final int last = Integer.parseInt(quantity);
         } catch (Exception ignored) {
         }
         Thread t=new Thread(){
@@ -49,9 +52,11 @@ public class BitcoinActivity extends AppCompatActivity implements View.OnClickLi
 
             @Override
             public void run(){
+                final float[] nb = {0};
                 while(!isInterrupted()){
+                    final float[] last = {nb[0]};
                     try {
-                        Thread.sleep(60000);  //1000ms = 1 sec
+                        Thread.sleep(10000);  //1000ms = 1 sec
                         runOnUiThread(new Runnable() {
 
                             @Override
@@ -63,6 +68,20 @@ public class BitcoinActivity extends AppCompatActivity implements View.OnClickLi
                                     JSONObject object = new JSONObject(jsonData);
                                     String quantity = object.getString("to_quantity");
                                     testBitcoin.setText("1€ = "+quantity+" btc");
+                                    nb[0] = Integer.parseInt(quantity);
+                                    if (((nb[0] - last[0]) > 50 && (nb[0] > last[0])) || ((last[0] - nb[0]) > 50 && (last[0] > nb[0]))) {
+                                        final OkHttpClient httpClient2 = new OkHttpClient();
+                                        final Request request = new Request.Builder()
+                                                .url("http://10.0.2.2:8080/send-mail")
+                                                .build();
+                                        RequestBody formBody = new FormBody.Builder()
+                                                .add("messageToSend", "La valeur du bitcoin a changé : "+ quantity + " !")
+                                                .add("subject_of_the_mail", "BITCOIN")
+                                                .add("receiver_mail", "lilian.brun@epitech.eu")
+                                                .build();
+                                        Response response2 = null;
+                                        response2 = httpClient2.newCall(request).execute();
+                                    }
                                 } catch (Exception ignored) {
                                 }
                             }
